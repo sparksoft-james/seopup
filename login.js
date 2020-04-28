434de37befc317512171e584c082616774d59927
sparksoft-james <52090276+sparksoft-james@users.noreply.github.com>
2020-04-28 16:49:18 +0800

    mutiple account login

--- login.js
+++ login.js
@@ -2,24 +2,29 @@ const path = require('path');
 const fs = require('fs');
 const pathToRegexp = require('path-to-regexp');
 const options = require('./config.js') || {};
-const { randGenNumber } = require('./libs/utils');
+const { getRandomArbitrary } = require('./libs/utils');
 
-async function login(page, credential) {
+
+async function login(page) {
+  // random choose account to login
+  const ranNum = getRandomArbitrary(0, 3);
+  console.log('using account:', ranNum);
+  const credential = options.credential[ranNum];
+
   console.log('browsing to https://www.facebook.com/...');
   await page.goto('https://facebook.com/');
+
   page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
   await page.$eval('#email', (el, value) => { el.value = value }, credential.username);
-
   await page.$eval('#pass', (el, value) => { el.value = value }, credential.pwd);
   console.log('credential input completed');
+
   await Promise.all([
     page.$eval('#loginbutton', btn => btn.click()),
     page.waitForNavigation({ waitUntil: 'networkidle0' }),
   ]);
-
   
-
   console.log('login success');
 }
 
